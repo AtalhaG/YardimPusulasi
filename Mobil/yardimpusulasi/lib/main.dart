600b3cc8eb7408fe850416eccdcf6d665221cccb
Dilara Çetiner <dilaracetiner07@gmail.com>
2025-07-08 22:23:22 +0300

    mobil yeni_kişi_ekleme sayfası tasarımı

--- Mobil/yardimpusulasi/lib/main.dart
+++ Mobil/yardimpusulasi/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'views/login_page.dart';
+import 'views/yeni_kisi_ekle.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -14,6 +15,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: const LoginPage());
+    return MaterialApp(home: const YeniKisiEklePage());
   }
 }
