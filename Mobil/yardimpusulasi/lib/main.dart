788cc7b91c75d61f8675cb50e5db4a499b2d7c1e
Talha Gürle <talha422634@gmail.com>
2025-07-11 11:40:48 +0300

    hatalar giderildi

--- Mobil/yardimpusulasi/lib/main.dart
+++ Mobil/yardimpusulasi/lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'views/login_page.dart';
-import 'views/yeni_kisi_ekle.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
